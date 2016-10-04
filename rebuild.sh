bb231c8a602032043940fe154d7245075e50921c
Fernando Tiberti <fertiberti@gmail.com>
2016-10-04 04:11:24 -0300

    Add rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,12 @@
+echo 'Removing previously running container'
+docker stop dash_insight
+docker rm dash_insight
+
+echo 'Building dashpay/bitcore_insight:1.1'
+docker build -t dashpay/bitcore_insight:1.1 .
+
+echo "Creating dash_insight"
+docker create -p 2052:3001 --name dash_insight dashpay/bitcore_insight:1.1
+
+echo "Running dash_insight"
+docker start dash_insight
