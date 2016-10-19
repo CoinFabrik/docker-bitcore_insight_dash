4378e92bd70f313c933db36a00723f363f0f1fae
Fernando Tiberti <fertiberti@gmail.com>
2016-10-19 16:55:38 -0300

    Fist build then remove previously running container

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,9 @@
+echo 'Building dashpay/bitcore_insight:1.1'
+docker build -t dashpay/bitcore_insight:1.1 .
+
 echo 'Removing previously running container'
 docker stop dash_insight
 docker rm dash_insight
-
-echo 'Building dashpay/bitcore_insight:1.1'
-docker build -t dashpay/bitcore_insight:1.1 .
 
 echo "Creating dash_insight"
 docker create -p 2052:3001 --volume $HOME/dash-data:$HOME/.bitcore --name dash_insight dashpay/bitcore_insight:1.1
